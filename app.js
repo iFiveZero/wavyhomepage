f88734cd6969f6ae1e8b6bce60eb0020bad12b13
iFiveZero <15315028+iFiveZero@users.noreply.github.com>
2023-05-29 14:25:39 +0100

    mouse effect

--- app.js
+++ app.js
@@ -1,21 +1,83 @@
+/*
 const track = document.getElementById("image-track");
-
-if (track) {
-  console.log(track.dataset.mouseDownAt);
-} else {
-  console.log("The element with the ID `wrapper` does not exist.");
-}
 
 window.onmousedown = e => {
    track.dataset.mouseDownAt = e.clientX;
-   console.log(e.clientX)
+}
+
+window.onmouseup = () => {
+    track.dataset.mouseDownAt = "0"
+    track.dataset.prevPercentage = track.dataset.percentage;
+   
 }
 
 window.onmousemove = e => {
-if(track.dataset.mouseDownAt === "0") return;
+    if(track.dataset.mouseDownAt === "0") return;
+
     const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
           maxDelta =  window.innerWidth / 2;  
     
-    const percentage = (mouseDelta / maxDelta) * 100;
-    track.style.transform = `translate(${percentage}%, -50%)`
+    const percentage = (mouseDelta / maxDelta) * -100,
+          nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage;
+          nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+
+    track.dataset.percentage = nextPercentage;
+
+    //track.style.transform = `translate(${nextPercentage}%, -50%)`; 
+    track.animate({
+        transform: `translate(${nextPercentage}%, -50%)`
+    }, { duration: 1200, fill: "forwards" });
+    
+    for(const image of track.getElementsByClassName("image")) {
+        image.animate({
+        objectPosition: `${100 + nextPercentage}% center`
+        }, { duration: 1200, fill: "forwards" });
+    }
 }
+
+*/
+const track = document.getElementById("image-track");
+
+const handleOnDown = e => track.dataset.mouseDownAt = e.clientX;
+
+const handleOnUp = () => {
+  track.dataset.mouseDownAt = "0";  
+  track.dataset.prevPercentage = track.dataset.percentage;
+}
+
+const handleOnMove = e => {
+  if(track.dataset.mouseDownAt === "0") return;
+  
+  const mouseDelta = parseFloat(track.dataset.mouseDownAt) - e.clientX,
+        maxDelta = window.innerWidth / 2;
+  
+  const percentage = (mouseDelta / maxDelta) * -100,
+        nextPercentageUnconstrained = parseFloat(track.dataset.prevPercentage) + percentage,
+        nextPercentage = Math.max(Math.min(nextPercentageUnconstrained, 0), -100);
+  
+  track.dataset.percentage = nextPercentage;
+  
+  track.animate({
+    transform: `translate(${nextPercentage}%, -50%)`
+  }, { duration: 1200, fill: "forwards" });
+  
+  for(const image of track.getElementsByClassName("image")) {
+    image.animate({
+      objectPosition: `${100 + nextPercentage}% center`
+    }, { duration: 1200, fill: "forwards" });
+  }
+}
+
+/* -- Had to add extra lines for touch events -- */
+
+window.onmousedown = e => handleOnDown(e);
+
+window.ontouchstart = e => handleOnDown(e.touches[0]);
+
+window.onmouseup = e => handleOnUp(e);
+
+window.ontouchend = e => handleOnUp(e.touches[0]);
+
+window.onmousemove = e => handleOnMove(e);
+
+window.ontouchmove = e => handleOnMove(e.touches[0]);
